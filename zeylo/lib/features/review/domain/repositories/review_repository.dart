5b3983ff4c6b013ff65feb80ee077bec54a9f4a3
bevanstef <bevansteferd2022@gmail.com>
2026-03-16 01:06:01 +0530

    added a new functional buttons to the review section

--- zeylo/lib/features/review/domain/repositories/review_repository.dart
+++ zeylo/lib/features/review/domain/repositories/review_repository.dart
@@ -3,4 +3,7 @@ import '../entities/review_entity.dart';
 abstract class ReviewRepository {
   Future<void> submitReview(ReviewEntity review);
   Future<List<ReviewEntity>> getReviewsForExperience(String experienceId);
+  Future<void> toggleHelpful(String reviewId, String userId);
+  Future<void> reportReview(String reviewId, String reporterId, String hostId);
+  Future<void> deleteReview(String reviewId);
 }
