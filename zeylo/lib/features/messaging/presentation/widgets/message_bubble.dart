cf1c4fe47753e4873e863db4db2bf840f3939b7f
Kavinu Wethmin <wethminkavinuwittahachchi123@gmail.com>
2026-03-20 09:24:44 +0530

    Test

--- zeylo/lib/features/messaging/presentation/widgets/message_bubble.dart
+++ zeylo/lib/features/messaging/presentation/widgets/message_bubble.dart
@@ -158,6 +158,7 @@ class MessageBubble extends StatelessWidget {
     final period = hour >= 12 ? 'PM' : 'AM';
     if (hour > 12) hour -= 12;
     if (hour == 0) hour = 12;
+
     return '$hour:$minute $period';
   }
 }
